296713cda31a901d43f5bb0cbf4fa41f83d6bfe7
Ciprian Ciurea <ciprianciurea@outlook.com>
2016-04-09 13:42:54 +0300

    created project

---
+++ ObservableEntitiesLightTracking/ObservableEntitiesLightTracking/Enums.cs
@@ -0,0 +1,11 @@
+﻿
+namespace ObservableEntitiesLightTracking
+{
+    public enum OEEntityState
+    {
+        Unchanged = 1,
+        Added = 2,
+        Deleted = 4,
+        Modified = 8
+    }
+}
