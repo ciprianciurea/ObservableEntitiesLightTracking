e70fa6e7db3efd8a0a2e968136fd30fad8a72e16
Ciprian Ciurea <ciprianciurea@outlook.com>
2016-04-09 14:33:56 +0300

    tests

--- ObservableEntitiesLightTracking/ObservableEntitiesLightTracking.Tests/OEContextTests.cs
+++ ObservableEntitiesLightTracking/ObservableEntitiesLightTracking.Tests/OEContextTests.cs
@@ -7,6 +7,7 @@ namespace ObservableEntitiesLightTracking.Tests
     [TestClass]
     public class OEContextTests
     {
+        #region Set<TEntity> tests
         [TestMethod]
         public void Set_returns_the_same_entity_set_for_the_same_entity_type()
         {
@@ -15,7 +16,9 @@ namespace ObservableEntitiesLightTracking.Tests
             var productSetAgain = context.Set<Product>(null);
             Assert.AreSame(productSet, productSetAgain);
         }
+        #endregion Set<TEntity> tests
 
+        #region  GetChanges tests
         [TestMethod]
         public void GetChanges_should_not_return_attached_unchanged()
         {
@@ -62,5 +65,93 @@ namespace ObservableEntitiesLightTracking.Tests
             var changes = context.GetChanges();
             Assert.AreSame(product, changes.FirstOrDefault());
         }
+
+        [TestMethod]
+        public void GetChanges_should_return_added_modified()
+        {
+            var context = new OEContext();
+            var product = new Product()
+            {
+                Id = 1,
+                Name = "Test product",
+                UnitPrice = 100
+            };
+            context.Set<Product>(null).Add(product);
+            product.UnitPrice++;
+
+            var changes = context.GetChanges();
+            Assert.AreSame(product, changes.FirstOrDefault());
+        }
+
+        [TestMethod]
+        public void GetChanges_should_return_attached_deleted()
+        {
+            var context = new OEContext();
+            var product = new Product()
+            {
+                Id = 1,
+                Name = "Test product",
+                UnitPrice = 100
+            };
+            context.Set<Product>(null).Attach(product);
+            context.Set<Product>(null).Delete(product);
+
+            var changes = context.GetChanges();
+            Assert.AreSame(product, changes.FirstOrDefault());
+        }
+
+        [TestMethod]
+        public void GetChanges_should_not_return_attached_modified_deleted()
+        {
+            var context = new OEContext();
+            var product = new Product()
+            {
+                Id = 1,
+                Name = "Test product",
+                UnitPrice = 100
+            };
+            context.Set<Product>(null).Attach(product);
+            product.UnitPrice++;
+            context.Set<Product>(null).Delete(product);
+
+            var changes = context.GetChanges();
+            Assert.AreSame(product, changes.FirstOrDefault());
+        }
+
+        [TestMethod]
+        public void GetChanges_should_not_return_added_deleted()
+        {
+            var context = new OEContext();
+            var product = new Product()
+            {
+                Id = 1,
+                Name = "Test product",
+                UnitPrice = 100
+            };
+            context.Set<Product>(null).Add(product);
+            context.Set<Product>(null).Delete(product);
+
+            var changes = context.GetChanges();
+            Assert.AreEqual(0, changes.Count());
+        }
+
+        [TestMethod]
+        public void GetChanges_should_not_return_added_modified_deleted()
+        {
+            var context = new OEContext();
+            var product = new Product()
+            {
+                Id = 1,
+                Name = "Test product",
+                UnitPrice = 100
+            };
+            context.Set<Product>(null).Add(product);
+            product.UnitPrice++;
+            context.Set<Product>(null).Delete(product);
+
+            var changes = context.GetChanges();
+            Assert.AreEqual(0, changes.Count());
+        }
+        #endregion  GetChanges tests
     }
 }
