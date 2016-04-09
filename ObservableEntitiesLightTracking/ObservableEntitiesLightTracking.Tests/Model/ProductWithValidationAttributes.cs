12cb8326ca49d8796aea39023d0c1dede400eae6
Ciprian Ciurea <ciprianciurea@outlook.com>
2016-04-09 18:09:55 +0300

    tests

---
+++ ObservableEntitiesLightTracking/ObservableEntitiesLightTracking.Tests/Model/ProductWithValidationAttributes.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ObservableEntitiesLightTracking.Tests.Model
+{
+    public class ProductWithValidationAttributes : ObservableObject
+    {
+        int _id;
+        public int Id
+        {
+            get { return _id; }
+            set { SetProperty(ref _id, value); }
+        }
+
+        string _name;
+        [Required]
+        public string Name
+        {
+            get { return _name; }
+            set { SetProperty(ref _name, value); }
+        }
+
+        private decimal _unitPrice;
+        [Range(0.0, Double.MaxValue)]
+        public decimal UnitPrice
+        {
+            get { return _unitPrice; }
+            set { SetProperty(ref _unitPrice, value); }
+        }
+    }
+}
--- ObservableEntitiesLightTracking/ObservableEntitiesLightTracking.Tests/ValidationTests.cs
+++ ObservableEntitiesLightTracking/ObservableEntitiesLightTracking.Tests/ValidationTests.cs
@@ -1,13 +1,90 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ObservableEntitiesLightTracking.ComponentModel;
+using ObservableEntitiesLightTracking.Tests.Model;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ObservableEntitiesLightTracking.Tests
 {
     [TestClass]
     public class ValidationTests
     {
+        #region OEContext validation tests
         [TestMethod]
-        public void TestMethod1()
+        public void Should_validate_when_no_conditions()
         {
+            var context = new OEContext();
+            var productSet = context.Set<Product>();
+            var product = new Product();
+            productSet.Add(product);
+            var result = context.Validate();
+            Assert.AreEqual(true, result);
         }
+
+        [TestMethod]
+        public void Should_validate_when_attributes_conditions_are_met()
+        {
+            var context = new OEContext();
+            var productSet = context.Set<ProductWithValidationAttributes>();
+            var product = new ProductWithValidationAttributes()
+            {
+                Id = 1,
+                Name = "Test product",
+                UnitPrice = 100
+            };
+            productSet.Add(product);
+            var result = context.Validate();
+            Assert.AreEqual(true, result);
+        }
+
+        [TestMethod]
+        public void Should_not_validate_when_attributes_conditions_are_not_met()
+        {
+            var context = new OEContext();
+            var productSet = context.Set<ProductWithValidationAttributes>();
+            var product = new ProductWithValidationAttributes()
+            {
+                Id = 1,
+                Name = null,
+                UnitPrice = -1
+            };
+            productSet.Add(product);
+            var result = context.Validate();
+            Assert.AreEqual(false, result);
+        }
+        #endregion OEContext validation tests
+
+        #region OEEntitySet validation tests
+        [TestMethod]
+        public void Should_validate_and_no_errors_when_no_conditions()
+        {
+            var context = new OEContext();
+            var productSet = context.Set<Product>();
+            var product = new Product();
+            productSet.Add(product);
+            var validationResults = new List<ValidationResultWithSeverityLevel>();
+            var result = productSet.Validate(validationResults);
+            Assert.AreEqual(true, result);
+            Assert.AreEqual(0, validationResults.Count());
+        }
+
+        [TestMethod]
+        public void Should_validate_and_no_errors_when_attributes_conditions_are_met()
+        {
+            var context = new OEContext();
+            var productSet = context.Set<ProductWithValidationAttributes>();
+            var product = new ProductWithValidationAttributes()
+            {
+                Id = 1,
+                Name = "Test product",
+                UnitPrice = 100
+            };
+            productSet.Add(product);
+            var validationResults = new List<ValidationResultWithSeverityLevel>();
+            var result = productSet.Validate(validationResults);
+            Assert.AreEqual(true, result);
+            Assert.AreEqual(0, validationResults.Count());
+        }
+        #endregion OEEntitySet validation tests
     }
 }
