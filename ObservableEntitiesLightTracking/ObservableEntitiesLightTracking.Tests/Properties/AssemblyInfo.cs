c260a90a20fc114ef161575635d14dae55464f05
Ciprian Ciurea <ciprianciurea@outlook.com>
2016-04-10 08:39:20 +0300

    version 1.0

--- ObservableEntitiesLightTracking/ObservableEntitiesLightTracking.Tests/Properties/AssemblyInfo.cs
+++ ObservableEntitiesLightTracking/ObservableEntitiesLightTracking.Tests/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ObservableEntitiesLightTracking.Tests")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Observable Entities Light Tracking and Validation Tests")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ObservableEntitiesLightTracking.Tests")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright © Ciprian Ciurea  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
--- ObservableEntitiesLightTracking/ObservableEntitiesLightTracking/Properties/AssemblyInfo.cs
+++ ObservableEntitiesLightTracking/ObservableEntitiesLightTracking/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ObservableEntitiesLightTracking")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Observable Entities Light Tracking and Validation")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ObservableEntitiesLightTracking")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright © Ciprian Ciurea  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
