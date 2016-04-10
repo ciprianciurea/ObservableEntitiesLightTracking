3f59349e5b847a00a79a68b76cf58e56a6ccf713
Ciprian Ciurea <ciprianciurea@outlook.com>
2016-04-10 08:27:28 +0300

    tests

---
+++ ObservableEntitiesLightTracking/ObservableEntitiesLightTracking.Tests/Model/ProductWithIWriteDataError.cs
@@ -0,0 +1,42 @@
+﻿using ObservableEntitiesLightTracking.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObservableEntitiesLightTracking.Tests.Model
+{
+    public class ProductWithIWriteDataError : ObservableObject, IWriteDataErrorInfo
+    {
+        int _id;
+        public int Id
+        {
+            get { return _id; }
+            set { SetProperty(ref _id, value); }
+        }
+
+        string _name;
+        [Required]
+        public string Name
+        {
+            get { return _name; }
+            set { SetProperty(ref _name, value); }
+        }
+
+        private decimal _unitPrice;
+        public decimal UnitPrice
+        {
+            get { return _unitPrice; }
+            set { SetProperty(ref _unitPrice, value); }
+        }
+
+        public ICollection<ValidationResultWithSeverityLevel> ValidationResults { get; set; }
+
+        public void AddValidationErrors(ICollection<ValidationResultWithSeverityLevel> validationResults)
+        {
+            ValidationResults = validationResults;
+        }
+    }
+}
---
+++ ObservableEntitiesLightTracking/ObservableEntitiesLightTracking.Tests/WriteDataErrorTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ObservableEntitiesLightTracking.Tests.Model;
+using ObservableEntitiesLightTracking.ComponentModel;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ObservableEntitiesLightTracking.Tests
+{
+    [TestClass]
+    public class WriteDataErrorTests
+    {
+        [TestMethod]
+        public void Should_write_validation_errors_to_the_entity_implementing_IWriteDataErrorInfo()
+        {
+            var context = new OEContext();
+            var productSet = context.Set<ProductWithIWriteDataError>();
+            var product = new ProductWithIWriteDataError();
+            productSet.Add(product);
+
+            var validationResults = new List<ValidationResultWithSeverityLevel>();
+            var result = productSet.Validate(validationResults);
+            Assert.AreEqual(false, result);
+            Assert.AreEqual(1, validationResults.Count());
+            Assert.AreSame(product, validationResults[0].Entity);
+            Assert.AreEqual("Name", validationResults[0].MemberNames.ElementAt(0));
+
+            var objectValidationResults = product.ValidationResults.ToList();
+            Assert.AreEqual("Name", objectValidationResults[0].MemberNames.ElementAt(0));
+        }
+    }
+}
